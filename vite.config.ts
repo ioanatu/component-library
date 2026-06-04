d08b2cdbfeb709237eecfb17e25700ea068e46a1
ioanatu <tatu.ioana@gmail.com>
2026-06-04 14:37:50 +0200

    Adjust setup for package library: - add unplugin-dts to emit types - exclude files from build - move react and react-dom to devDependencies and add peerDependencies - update vite.config file for library project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "unplugin-dts/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ tsconfigPath: "tsconfig.build.json" })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: { globals: { react: "React" } },
+    },
+  },
 });
