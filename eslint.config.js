3038350b6b609d65e1e5c16d5857dd06a91d1031
ioanatu <tatu.ioana@gmail.com>
2026-06-06 20:42:37 +0200

    Exclude directories from eslint.config, add optional peerDependencies

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import storybook from 'eslint-plugin-storybook';
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 
@@ -21,12 +20,10 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
       'jsx-a11y': jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       ...jsxA11y.configs.recommended.rules,
     },
   },
