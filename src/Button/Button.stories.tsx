142c09db0ddf5c09592a1348f807cadb567ad4f4
ioanatu <tatu.ioana@gmail.com>
2026-09-03 18:16:59 +0200

    Remove superfluous story

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -55,8 +55,6 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
-
 export const SmallMediumLargeButtons: Story = {
   render: (args) => (
     <div style={{ margin: 'auto' }}>
