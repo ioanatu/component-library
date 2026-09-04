006de0f67f5b0d3d9631267b507fcc59221b6b38
ioanatu <tatu.ioana@gmail.com>
2026-09-04 13:14:01 +0200

    Add forgotten button variants

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export const buttonSizes = ['sm', 'md', 'lg'] as const;
 export type ButtonSize = (typeof buttonSizes)[number];
 
-export const buttonVariants = ['primary', 'secondary', 'danger'];
+export const buttonVariants = ['primary', 'secondary', 'danger', 'success', 'accent'];
 export type ButtonVariant = (typeof buttonVariants)[number];
